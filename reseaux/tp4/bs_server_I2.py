a2dd52c83091a3dd7fce8fb8b5e0ffe339a05186
Mattéo Verdez <matteo.verdez@ynov.com>
2023-11-06 11:59:22 +0100

    jtest un truc

--- reseaux/tp4/bs_server_I2.py
+++ reseaux/tp4/bs_server_I2.py
@@ -16,7 +16,7 @@ while True:
         data = conn.recv(1024)
         if not data: break
         if (data.decode() == "meo"):
-            conn.sendall("Meo à toi confrère.".encode())
+            conn.sendall("Meo à toi confrère.")
             break
         elif (data.decode() == "waf"):
             conn.sendall("ptdr t ki".encode())
