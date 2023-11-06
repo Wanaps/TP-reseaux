2b46dd0a289f20535202951e0071e02e7847cf0b
Mattéo Verdez <matteo.verdez@ynov.com>
2023-11-06 09:40:25 +0100

    ADD bs_server_l1.py

---
+++ reseaux/tp4/bs_server_I1.py
@@ -0,0 +1,25 @@
+import socket
+
+host = ''
+port = 13337
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
+
+s.listen(1)
+conn, addr = s.accept()
+print('Connected by', addr)
+
+while True:
+
+    try:
+        data = conn.recv(1024)
+        if not data: break
+        print(f"Données reçues: {data}")
+        conn.sendall("Salut mec.")
+
+    except socket.error:
+        print("Erreur de connexion.")
+        break
+
+conn.close()
