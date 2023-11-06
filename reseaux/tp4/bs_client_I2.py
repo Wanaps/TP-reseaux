ece3bda505efc4ec5783a7c2d92f097def732ddf
Mattéo Verdez <matteo.verdez@ynov.com>
2023-11-06 11:47:44 +0100

    client l2 test

---
+++ reseaux/tp4/bs_client_I2.py
@@ -0,0 +1,25 @@
+import socket
+import sys
+
+host = '10.1.1.11'
+port = 13337
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect((host, port))
+
+txt = input("Que veux-tu envoyer au serveur : ")
+
+
+try:
+    s.sendall(txt)
+    data = s.recv(1024)
+    print(f"Connecté avec succès au serveur {host} sur le port {port}")
+except socket.error:
+    print("pas de bol anatole, ça a foiré")
+    sys.exit(1)
+
+
+print(f"réponse : {repr(data)}")
+
+sys.exit(0)
